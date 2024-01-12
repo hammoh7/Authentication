537f79e754c11179abee3cdd356159c7913de06c
hammoh7 <soahammohaadkar@gmail.com>
2024-01-12 11:38:15 +0530

    Routes

---
+++ routes.ts
@@ -0,0 +1,31 @@
+/*
+ * An array of routes that are accessible publicly
+ * These routes do not require authentication
+ * @type {string[]} 
+*/
+export const publicRoutes = [
+    "/",
+];
+
+/*
+ * An array of routes that are used for authentication
+ * These routes redirects logged in users to /settings
+ * @type {string[]} 
+*/
+export const authRoutes = [
+    '/Login',
+    '/Register',
+];
+
+/*
+ * The prefix for API authentication reoutes 
+ * Routes that start with this prefix are used for API authentication purposes
+ * @type {string} 
+*/
+export const apiAuthPrefix = "/api/auth";
+
+/*
+ * The default redirect path after logging in
+ * @type {string} 
+*/
+export const Default_Login_Redirect = "/settings"
