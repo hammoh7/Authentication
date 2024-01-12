391234496484883883a0accc42d7c455c96b63b2
hammoh7 <soahammohaadkar@gmail.com>
2024-01-12 11:40:32 +0530

    Actions

--- activities/login.ts
+++ activities/login.ts
@@ -1,13 +1,33 @@
 "use server";
 
-import { revalidatePath, revalidateTag } from "next/cache";
 import *  as z  from "zod";
 import { LoginSchema } from "@/schemas";
+import { signIn, signOut } from "@/auth";
+import { Default_Login_Redirect } from "@/routes";
+import { AuthError } from "next-auth";
 
 export const login = async (values:z.infer<typeof LoginSchema>) => {
     const validatedFields = LoginSchema.safeParse(values);
     if(!validatedFields.success) {
         return { error: "Invalid credentials!" };
     }
-    return { success: "Verified successfully!" };
+    const { email, password } = validatedFields.data;
+
+    try {
+        await signIn("credentials", {
+            email,
+            password,
+            redirectTo: Default_Login_Redirect
+        }) 
+    } catch (error) {
+        if (error instanceof AuthError) {
+            switch(error.type) {
+                case "CredentialsSignin":
+                    return { error: "Invalid credentials!" }
+                default:
+                    return { error: "Something went wrong!" }
+            }
+        }
+        throw error;
+    }
 };
--- activities/register.ts
+++ activities/register.ts
@@ -1,13 +1,33 @@
 "use server";
 
 import { revalidatePath, revalidateTag } from "next/cache";
-import *  as z  from "zod";
+import * as z from "zod";
+import bcrypt from "bcryptjs";
+import { database } from "@/lib/database";
 import { RegisterSchema } from "@/schemas";
+import { getUserByMail } from "@/data/user";
 
-export const register = async (values:z.infer<typeof RegisterSchema>) => {
-    const validatedFields = RegisterSchema.safeParse(values);
-    if(!validatedFields.success) {
-        return { error: "Invalid credentials!" };
-    }
-    return { success: "Verified successfully!" };
+export const register = async (values: z.infer<typeof RegisterSchema>) => {
+  const validatedFields = RegisterSchema.safeParse(values);
+  if (!validatedFields.success) {
+    return { error: "Invalid credentials!" };
+  }
+  const { name, email, password } = validatedFields.data;
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const existingUser = await getUserByMail(email);
+
+  if (existingUser) {
+    return { error: "Email already exists!" };
+  }
+  await database.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+    },
+  });
+
+  // To send verification token email
+
+  return { success: "Account created successfully!" };
 };
